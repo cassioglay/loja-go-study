bf595bcc243b529327ba157d33b075a5c30476ac
cassio.glay <cassio.glay#locktec.com.br>
2024-04-26 18:37:11 -0300

    feat: Connect DB - add config. and connection on database

--- main.go
+++ main.go
@@ -1,11 +1,41 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
 	"text/template"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+func conectaComBancoDeDados() *sql.DB {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db_user := os.Getenv("POSTGRES_USER")
+	db_password := os.Getenv("POSTGRES_PASSWORD")
+	database := os.Getenv("POSTGRES_DB")
+	db_host := os.Getenv("DB_HOST")
+
+	conexao := "user=" + db_user + " dbname=" + database + " password=" + db_password + " host=" + db_host + " sslmode=disable"
+
+	db, err := sql.Open("postgres", conexao)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
+
 type Produto struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -16,22 +46,43 @@ type Produto struct {
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
 // Executa o template a partir do temp
 func index(w http.ResponseWriter, r *http.Request) {
+	db := conectaComBancoDeDados()
 
-	//Lista exemplo
-	produtos := []Produto{
-		{
-			Nome:       "Camiseta",
-			Descricao:  "Camiseta azul bem bonita",
-			Preco:      39.0,
-			Quantidade: 5,
-		},
+	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectDeTodosOsProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err := selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
 	}
 
 	temp.ExecuteTemplate(w, "Index", produtos)
+	defer db.Close()
 }
