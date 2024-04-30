e3daff573c21a41daeb149e09b5d71aa7b907d35
cassio.glay <cassio.glay#locktec.com.br>
2024-04-30 16:59:17 -0300

    feat - New product - add new product to product list

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -13,3 +13,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	produtos := models.BuscaTodosOsProdutos()
 	temp.ExecuteTemplate(w, "Index", produtos)
 }
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
